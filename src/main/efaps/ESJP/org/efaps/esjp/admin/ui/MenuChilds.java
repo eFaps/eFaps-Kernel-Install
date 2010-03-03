08154fa84bfe76f7aeb834d003ddb4d6ce504686
miguel.a.aranya <miguel.a.aranya@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2010-03-03 15:37:39 +0000

    - rework of menuchild up-down funcionality
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-Kernel-Install/trunk@3794 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/admin/ui/MenuChilds.java
+++ src/main/efaps/ESJP/org/efaps/esjp/admin/ui/MenuChilds.java
@@ -26,11 +26,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.efaps.admin.event.EventExecution;
 import org.efaps.admin.event.Parameter;
 import org.efaps.admin.event.Return;
 import org.efaps.admin.event.Parameter.ParameterValues;
-import org.efaps.admin.event.Return.ReturnValues;
 import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
 import org.efaps.db.Instance;
@@ -47,37 +45,8 @@ import org.efaps.util.EFapsException;
  */
 @EFapsUUID("f03932cf-7227-46c9-bb92-546245b1f49b")
 @EFapsRevision("$Rev$")
-public class MenuChilds implements EventExecution
+public class MenuChilds
 {
-
-    /**
-     * Method is used to retrieve the relationship id of the childs, so that the
-     * table can be sorted in the same order as they are rendered from the
-     * webapp. It is called as an UI_FIELD_VALUE event from within table
-     * Admin_UI_Menu_Childs.
-     *
-     * @param _parameter Parameter as provided to the esjp from eFaps
-     * @throws EFapsException on error
-     * @return Return for eFaps
-     */
-    public Return execute(final Parameter _parameter) throws EFapsException
-    {
-        final Instance instance = (Instance) _parameter.get(ParameterValues.INSTANCE);
-        final Instance callinstance = (Instance) _parameter.get(ParameterValues.CALL_INSTANCE);
-
-        final SearchQuery query = new SearchQuery();
-        query.setQueryTypes("Admin_UI_Menu2Command");
-        query.addWhereExprEqValue("FromMenu", callinstance.getId());
-        query.addWhereExprEqValue("ToCommand", instance.getId());
-        query.addSelect("ID");
-        query.execute();
-
-        final Return retVal = new Return();
-        if (query.next()) {
-            retVal.put(ReturnValues.VALUES, query.get("ID"));
-        }
-        return retVal;
-    }
 
     /**
      * Method is called from a command to move the child of a menu on position
@@ -88,7 +57,8 @@ public class MenuChilds implements EventExecution
      * @throws EFapsException on error
      * @see #move(Parameter, boolean)
      */
-    public Return moveChildUp(final Parameter _parameter) throws EFapsException
+    public Return moveChildUp(final Parameter _parameter)
+        throws EFapsException
     {
         move(_parameter, true);
         return new Return();
@@ -103,7 +73,8 @@ public class MenuChilds implements EventExecution
      * @throws EFapsException on error
      * @see #move(Parameter, boolean)
      */
-    public Return moveChildDown(final Parameter _parameter) throws EFapsException
+    public Return moveChildDown(final Parameter _parameter)
+        throws EFapsException
     {
         move(_parameter, false);
         return new Return();
@@ -116,65 +87,56 @@ public class MenuChilds implements EventExecution
      * @param _up move the child up
      * @throws EFapsException on error
      */
-    private void move(final Parameter _parameter, final boolean _up) throws EFapsException
+    private void move(final Parameter _parameter,
+                      final boolean _up)
+        throws EFapsException
     {
+        final Instance instance = _parameter.getInstance();
         final String[] oidRows = (String[]) _parameter.get(ParameterValues.OTHERS);
-        if (oidRows != null) {
+        if (oidRows != null && oidRows.length > 0) {
 
-            for (final String oidRow : oidRows) {
-                final String[] oids = oidRow.split("\\|");
+            final Instance selected = Instance.get(oidRows[0]);
+            final SearchQuery query2 = new SearchQuery();
+            query2.setQueryTypes("Admin_UI_Menu2Command");
+            query2.addWhereExprEqValue("FromMenu", instance.getId());
+            query2.addSelect("ID");
+            query2.addSelect("ToCommand");
+            query2.execute();
+            final List<Long> ids = new ArrayList<Long>();
+            final Map<Long, Long> id2cmd = new HashMap<Long, Long>();
+            while (query2.next()) {
+                final Long id = (Long) query2.get("ID");
+                final Long cmdId = (Long) query2.get("ToCommand");
+                ids.add(id);
+                id2cmd.put(id, cmdId);
+            }
+            Collections.sort(ids);
+            for (int i = 0; i < ids.size(); i++) {
+                final Long actId = ids.get(i);
+                final Long actCmd = id2cmd.get(actId);
 
-                final String selCmdId = oids[1].split("\\.")[1];
-                final SearchQuery query = new SearchQuery();
-                query.setObject(oids[0]);
-                query.addSelect("FromMenu");
-                query.execute();
-                if (query.next()) {
-                    final String menuid = query.get("FromMenu").toString();
-
-                    final SearchQuery query2 = new SearchQuery();
-                    query2.setQueryTypes("Admin_UI_Menu2Command");
-                    query2.addWhereExprEqValue("FromMenu", menuid);
-                    query2.addSelect("ID");
-                    query2.addSelect("ToCommand");
-                    query2.execute();
-                    final List<Long> ids = new ArrayList<Long>();
-                    final Map<Long, Long> id2cmd = new HashMap<Long, Long>();
-                    while (query2.next()) {
-                        final Long id = (Long) query2.get("ID");
-                        final Long cmdId = (Long) query2.get("ToCommand");
-                        ids.add(id);
-                        id2cmd.put(id, cmdId);
+                if (actId.equals(selected.getId())) {
+                    Long exId = null;
+                    // if move upwards and the selected is not the first
+                    if (_up && i > 0) {
+                        exId = ids.get(i - 1);
+                    } else if (!_up && i < ids.size() - 1) {
+                        exId = ids.get(i + 1);
                     }
-                    Collections.sort(ids);
-                    for (int i = 0; i < ids.size(); i++) {
-                        final Long actId = ids.get(i);
-                        final Long actCmd = id2cmd.get(actId);
+                    if (exId != null) {
+                        // update the exChange
+                        final Update update = new Update("Admin_UI_Menu2Command", exId.toString());
+                        update.add("ToCommand", actCmd);
+                        update.execute();
+                        update.close();
 
-                        if (actCmd.equals(Long.parseLong(selCmdId))) {
-                            Long exId = null;
-                            // if move upwards and the selected is not the first
-                            if (_up && i > 0) {
-                                exId = ids.get(i - 1);
-                            } else if (!_up && i < ids.size() - 1) {
-                                exId = ids.get(i + 1);
-                            }
-                            if (exId != null) {
-                                // update the exChange
-                                final Update update = new Update("Admin_UI_Menu2Command", exId.toString());
-                                update.add("ToCommand", actCmd);
-                                update.execute();
-                                update.close();
-
-                                // update the actual
-                                final Update updateAct = new Update("Admin_UI_Menu2Command", actId.toString());
-                                updateAct.add("ToCommand", id2cmd.get(exId));
-                                updateAct.execute();
-                                updateAct.close();
-                            }
-                            break;
-                        }
+                        // update the actual
+                        final Update updateAct = new Update("Admin_UI_Menu2Command", actId.toString());
+                        updateAct.add("ToCommand", id2cmd.get(exId));
+                        updateAct.execute();
+                        updateAct.close();
                     }
+                    break;
                 }
             }
         }
