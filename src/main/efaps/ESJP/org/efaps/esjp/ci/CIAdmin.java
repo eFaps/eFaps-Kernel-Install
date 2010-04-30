d1ea959724ef07205d00fbdbe75aad3d07dd27c2
jan.moxter <jan.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2010-04-30 00:21:37 +0000

    - adding manual ci esjp for kernel install, changing to current kernel
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-Kernel-Install/trunk@4308 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/ci/CIAdmin.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2003 - 2010 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.esjp.ci;
+
+import org.efaps.ci.CIAttribute;
+import org.efaps.ci.CIType;
+
+/**
+ * TODO comment!
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+public class CIAdmin
+{
+
+    public static final _DataModel_StatusAbstract DataModel_StatusAbstract = new _DataModel_StatusAbstract(
+                    "f3eaf2f3-b24c-43c0-9ea1-0f6354438c81");
+
+    public static class _DataModel_StatusAbstract
+        extends CIType
+    {
+
+        protected _DataModel_StatusAbstract(final String _uuid)
+        {
+            super(_uuid);
+        }
+
+        public final CIAttribute Key = new CIAttribute(this, "Key");
+        public final CIAttribute Description = new CIAttribute(this, "Description");
+    }
+}
