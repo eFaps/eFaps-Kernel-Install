7501500343c86732b6ccc917ac1d3031adcbf669
jan.moxter <jan.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2010-07-14 16:49:13 +0000

    - adding new basic esjp for fields in a form to kernel install
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-Kernel-Install/trunk@4972 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/common/uiform/Field_Base.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2003 - 2010 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.esjp.common.uiform;
+
+import java.util.Map;
+
+import org.efaps.admin.datamodel.ui.FieldValue;
+import org.efaps.admin.datamodel.ui.UIInterface;
+import org.efaps.admin.event.Parameter;
+import org.efaps.admin.event.Return;
+import org.efaps.admin.event.Parameter.ParameterValues;
+import org.efaps.admin.event.Return.ReturnValues;
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.util.EFapsException;
+
+/**
+ * Class contains basic methods used to render standard Fields that are not
+ * based on an attribute.
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+@EFapsUUID("92337601-c2df-4f78-bb80-9c9b8b81c35c")
+@EFapsRevision("$Rev$")
+public abstract class Field_Base
+{
+    /**
+     * Render a single checkbox.<br>
+     * Properties:
+     * <table>
+     *  <tr><th>Property</th><th>Value</th><th>Default</th></tr>
+     *  <tr><td>checked</td><td>true, false</td><td>false</td></tr>
+     *  <tr><td>value</td><td>any String</td><td>"true"</td></tr>
+     * </table>
+     *
+     * @param _parameter    Parameter as passed from the eFaps API
+     * @return html snipplet for a checkbox
+     * @throws EFapsException on error
+     */
+    public Return checkboxFieldValue(final Parameter _parameter)
+        throws EFapsException
+    {
+        final StringBuilder html = new StringBuilder();
+        final FieldValue fieldValue = (FieldValue) _parameter.get(ParameterValues.UIOBJECT);
+
+        final Map<?, ?> props = (Map<?, ?>) _parameter.get(ParameterValues.PROPERTIES);
+        final boolean checked = "true".equalsIgnoreCase((String) props.get("checked"));
+        final String value = props.containsKey("value") ? (String) props.get("value") : "true";
+
+        html.append("<input type=\"checkbox\" name=\"").append(fieldValue.getField().getName()).append("\" ")
+                        .append(UIInterface.EFAPSTMPTAG).append(" value=\"").append(value).append("\" ");
+        if (checked) {
+            html.append(" checked=\"checked\" ");
+        }
+        html.append("/>");
+        final Return ret = new Return();
+        ret.put(ReturnValues.SNIPLETT, html.toString());
+        return ret;
+    }
+}
