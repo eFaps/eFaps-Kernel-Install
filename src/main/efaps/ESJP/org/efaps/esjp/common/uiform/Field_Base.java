cf10384f89d25681429f9e9338bab2904f7abfdb
jan.moxter <jan.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2010-09-22 17:27:42 +0000

    - adding method to format the value of a classification field in a table
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-Kernel-Install/trunk@5566 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/common/uiform/Field_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/common/uiform/Field_Base.java
@@ -20,17 +20,21 @@
 
 package org.efaps.esjp.common.uiform;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.UUID;
-import java.util.Map.Entry;
 
 import org.efaps.admin.common.SystemConfiguration;
+import org.efaps.admin.datamodel.Classification;
 import org.efaps.admin.datamodel.ui.FieldValue;
 import org.efaps.admin.datamodel.ui.UIInterface;
 import org.efaps.admin.event.Parameter;
-import org.efaps.admin.event.Return;
 import org.efaps.admin.event.Parameter.ParameterValues;
+import org.efaps.admin.event.Return;
 import org.efaps.admin.event.Return.ReturnValues;
 import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
@@ -82,7 +86,7 @@ public abstract class Field_Base
     }
 
     /**
-     * Renders a field taht contains the values from a
+     * Renders a field that contains the values from a
      * SystemConfigurationObjectAttribute.
      * Properties:
      * <table>
@@ -116,6 +120,51 @@ public abstract class Field_Base
         }
         final Return ret = new Return();
         ret.put(ReturnValues.SNIPLETT, html.toString());
+        return ret;
+    }
+
+    /**
+     * Formats the values for a class select field used normally in tables.
+     * Properties:
+     * <table>
+     *  <tr><th>Property</th><th>Value</th><th>Obligatory</th></tr>
+     *  <tr><td>Seperator</td><td>Seperator used to seprerate the values, if mor than one</td><td>false</td></tr>
+     * </table>
+      * @param _parameter    Parameter as passed from the eFaps API
+     * @return formated object
+     * @throws EFapsException on error
+     */
+    public Return classificationFieldFormat(final Parameter _parameter)
+        throws EFapsException
+    {
+        final StringBuilder html = new StringBuilder();
+        final Map<?, ?> props = (Map<?, ?>) _parameter.get(ParameterValues.PROPERTIES);
+        final String seperator = props.containsKey("Seperator") ? (String) props.get("Seperator") : ", ";
+        final FieldValue fieldValue = (FieldValue) _parameter.get(ParameterValues.UIOBJECT);
+        if (fieldValue != null) {
+            final Object value = fieldValue.getValue();
+            if (value instanceof List) {
+                final List<String> labels = new ArrayList<String>();
+                for (final Object val : (List<?>) value) {
+                    labels.add(((Classification) val).getLabel());
+                }
+                Collections.sort(labels);
+                boolean first = true;
+                for (final String label : labels) {
+                    if (first) {
+                        first = false;
+                    } else {
+                        html.append(seperator);
+                    }
+                    html.append(label);
+                }
+            } else if (value instanceof Classification) {
+                html.append(((Classification) value).getLabel());
+            }
+        }
+
+        final Return ret = new Return();
+        ret.put(ReturnValues.VALUES, html.toString());
         return ret;
     }
 }
