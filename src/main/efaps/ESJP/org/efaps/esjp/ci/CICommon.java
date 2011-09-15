837d7a683e738aa24a9853d2882b65c946e0686e
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2011-09-15 04:17:04 +0000

    - total rework of history in kernel-instal
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-Kernel-Install/trunk@7122 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/ci/CICommon.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2003 - 2011 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.esjp.ci;
+
+import org.efaps.ci.CIAttribute;
+import org.efaps.ci.CIType;
+
+/**
+ * TODO comment!
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+// CHECKSTYLE:OFF
+public class CICommon
+{
+
+    public static final _HistoryAbstract HistoryAbstract = new _HistoryAbstract("66a75f87-1e07-410d-a0c4-c6ec1671b9a1");
+
+    public static class _HistoryAbstract
+        extends CIType
+    {
+
+        protected _HistoryAbstract(final String _uuid)
+        {
+            super(_uuid);
+        }
+
+        public final CIAttribute GeneralInstanceLink = new CIAttribute(this, "GeneralInstanceLink");
+        public final CIAttribute Value = new CIAttribute(this, "Value");
+        public final CIAttribute Creator = new CIAttribute(this, "Creator");
+        public final CIAttribute Created = new CIAttribute(this, "Created");
+    }
+
+    public static final _HistoryCreate HistoryCreate = new _HistoryCreate("510ced6e-101a-434c-b891-069861e64fc2");
+
+    public static class _HistoryCreate
+        extends _HistoryAbstract
+    {
+
+        protected _HistoryCreate(final String _uuid)
+        {
+            super(_uuid);
+        }
+    }
+
+    public static final _HistoryUpdate HistoryUpdate = new _HistoryUpdate("fdc408a6-c94c-425c-86fa-6d632281010d");
+
+    public static class _HistoryUpdate
+        extends _HistoryAbstract
+    {
+
+        protected _HistoryUpdate(final String _uuid)
+        {
+            super(_uuid);
+        }
+    }
+}
--- src/main/efaps/ESJP/org/efaps/esjp/common/history/HistoryTrigger.java
+++ src/main/efaps/ESJP/org/efaps/esjp/common/history/HistoryTrigger.java
@@ -20,20 +20,27 @@
 
 package org.efaps.esjp.common.history;
 
-import java.util.Iterator;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import org.efaps.admin.datamodel.Attribute;
-import org.efaps.admin.event.EventExecution;
+import org.efaps.admin.datamodel.Status;
+import org.efaps.admin.datamodel.Type;
+import org.efaps.admin.datamodel.attributetype.StatusType;
+import org.efaps.admin.dbproperty.DBProperties;
 import org.efaps.admin.event.Parameter;
-import org.efaps.admin.event.Return;
 import org.efaps.admin.event.Parameter.ParameterValues;
+import org.efaps.admin.event.Return;
 import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
-import org.efaps.db.Context;
 import org.efaps.db.Insert;
 import org.efaps.db.Instance;
+import org.efaps.db.PrintQuery;
+import org.efaps.db.SelectBuilder;
+import org.efaps.esjp.ci.CICommon;
+import org.efaps.esjp.common.history.HistoryXML.AttrValue;
 import org.efaps.util.EFapsException;
 
 /**
@@ -44,56 +51,104 @@ import org.efaps.util.EFapsException;
 @EFapsUUID("11b5668b-de34-4cb5-985c-b3f10686e72c")
 @EFapsRevision("$Rev$")
 public class HistoryTrigger
-    implements EventExecution
 {
 
     /**
-     * @param _parameter
+     * @param _parameter Parameter as passed by the eFaps API
+     * @return new Return
+     * @throws EFapsException on error
      */
-    public Return execute(final Parameter _parameter)
+    public Return insertTrigger(final Parameter _parameter)
+        throws EFapsException
+    {
+        execute(_parameter, CICommon.HistoryCreate.getType(), "create");
+        return new Return();
+    }
+
+    /**
+     * @param _parameter Parameter as passed by the eFaps API
+     * @return new Return
+     * @throws EFapsException on error
+     */
+    public Return updateTrigger(final Parameter _parameter)
+        throws EFapsException
+    {
+        execute(_parameter, CICommon.HistoryUpdate.getType(), "update");
+        return new Return();
+    }
+
+    /**
+     * @param _parameter    Parameter as passed by the eFaps API
+     * @param _type         type to be created
+     * @param _event        event name
+     * @throws EFapsException on error
+     */
+    protected void execute(final Parameter _parameter,
+                           final Type _type,
+                           final String _event)
         throws EFapsException
     {
         final Instance instance = (Instance) _parameter.get(ParameterValues.INSTANCE);
         final Map<?, ?> values = (Map<?, ?>) _parameter.get(ParameterValues.NEW_VALUES);
-        final Map<?, ?> properties =
-                        (Map<?, ?>) _parameter.get(ParameterValues.PROPERTIES);
-        final String type = (String) properties.get("Type");
-
-        Insert insert = new Insert(type);
-        insert.add("ForID", ((Long) instance.getId()).toString());
-        insert.add("ForType", ((Long) instance.getType().getId()).toString());
-        if ("Common_History_AddChild".equals(type)
-                        || "Common_History_RemoveChild".equals(type)) {
-            final Context context = Context.getThreadContext();
-            final String oid = context.getParameter("oid");
-            final String typeid = oid.substring(0, oid.indexOf("."));
-            final String toid = oid.substring(oid.indexOf(".") + 1);
-            insert.add("ToType", typeid);
-            insert.add("ToID", toid);
-        }
-
-        insert.execute();
-        final Long ID = insert.getId();
-
-        insert.close();
-
-        if (values != null) {
-            final Iterator<?> iter = values.entrySet().iterator();
-
-            while (iter.hasNext()) {
-                final Entry<?, ?> entry = (Entry<?, ?>) iter.next();
-                final Attribute attr = (Attribute) entry.getKey();
-                final String value = entry.getValue().toString();
-
-                insert = new Insert("Common_History_Attributes");
-                insert.add("HistoryID", ID);
-                insert.add("Attribute", ((Long) attr.getId()).toString());
-                insert.add("Value", value);
-                insert.execute();
-                insert.close();
+        final Map<?, ?> properties = (Map<?, ?>) _parameter.get(ParameterValues.PROPERTIES);
+        // analyze the attributes
+        final Set<AttrValue> attrValues = new HashSet<AttrValue>();
+        for (final Entry<?, ?> entry : values.entrySet()) {
+            final Attribute attr = ((Attribute) entry.getKey());
+            if (properties.containsKey("Select4_" + attr.getName())) {
+                final String sel = (String) properties.get("Select4_" + attr.getName());
+                final AttrValue attrValue = new AttrValue(attr.getName(), DBProperties.getProperty(attr.getLabelKey(),
+                                "en"), sel);
+                attrValues.add(attrValue);
+            } else if (attr.getAttributeType().getDbAttrType() instanceof StatusType) {
+                final Long statusID = (Long) ((Object[]) entry.getValue())[0];
+                final Status status = Status.get(statusID);
+                attrValues.add(new AttrValue(attr.getName(), DBProperties.getProperty(attr.getLabelKey(), "en"),
+                                DBProperties.getProperty(status.getLabelKey(), "en"), null));
+            } else {
+                final Object val = entry.getValue();
+                final String value;
+                if (val instanceof Object[]) {
+                    value = ((Object[]) val)[0] == null ? null : ((Object[]) val)[0].toString();
+                } else {
+                    value = val == null ? null : val.toString();
+                }
+                attrValues.add(new AttrValue(attr.getName(), DBProperties.getProperty(attr.getLabelKey(), "en"), value,
+                                null));
             }
         }
+        final PrintQuery print = new PrintQuery(instance);
+        boolean exec = false;
+        for (final AttrValue attrValue : attrValues) {
+            if (attrValue.getSelect() != null) {
+                print.addSelect(attrValue.getSelect());
+                print.addSelect(new SelectBuilder().linkto(attrValue.getName()).oid());
+                exec = true;
+            }
+        }
+        if (exec && print.executeWithoutAccessCheck()) {
+            for (final AttrValue attrValue : attrValues) {
+                if (attrValue.getSelect() != null) {
+                    final String linkOid = print.<String>getSelect(new SelectBuilder().linkto(attrValue.getName())
+                                    .oid());
+                    attrValue.setLinkOid(linkOid);
+                    final Object val = print.getSelect(attrValue.getSelect());
+                    if (val != null) {
+                        attrValue.setValue(val.toString());
+                    }
+                }
+            }
+        }
+        // create the actual xml
+        final HistoryXML xml = new HistoryXML(instance, _event);
+        xml.addAttributes(attrValues);
+        final String xmlStr = xml.getXML();
 
-        return null;
+        if (instance.isValid() && instance.getType().isGeneralInstance()) {
+            final Insert insert = new Insert(_type);
+            insert.add(CICommon.HistoryAbstract.GeneralInstanceLink, instance.getGeneralId());
+            insert.add(CICommon.HistoryAbstract.Value, xmlStr);
+            insert.executeWithoutAccessCheck();
+        }
     }
 }
