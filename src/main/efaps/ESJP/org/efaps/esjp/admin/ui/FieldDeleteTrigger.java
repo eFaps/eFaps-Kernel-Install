db1b72792213346b797520bfdc06d50ed07630d0
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2013-02-19 15:26:21 +0000

    -  kernel-install: update of esjp to use QueryBuilder
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-Kernel-Install/trunk@8845 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/admin/ui/FieldDeleteTrigger.java
+++ src/main/efaps/ESJP/org/efaps/esjp/admin/ui/FieldDeleteTrigger.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003 - 2010 The eFaps Team
+ * Copyright 2003 - 2013 The eFaps Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,9 +25,13 @@ import org.efaps.admin.event.Parameter;
 import org.efaps.admin.event.Return;
 import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.ci.CIAdminCommon;
+import org.efaps.ci.CIAdminEvent;
+import org.efaps.ci.CIAdminUserInterface;
 import org.efaps.db.Delete;
 import org.efaps.db.Instance;
-import org.efaps.db.SearchQuery;
+import org.efaps.db.InstanceQuery;
+import org.efaps.db.QueryBuilder;
 import org.efaps.util.EFapsException;
 
 /**
@@ -38,53 +42,53 @@ import org.efaps.util.EFapsException;
  */
 @EFapsUUID("801f350c-3cdf-4a60-b4ee-19b8a63ec867")
 @EFapsRevision("$Rev$")
-public class FieldDeleteTrigger implements EventExecution {
+public class FieldDeleteTrigger
+    implements EventExecution
+{
+    /**
+     * Method is executed from the DELETE_PRE trigger of the type
+     * Admin_UI_Field, Admin_UI_FieldSet, Admin_UI_FieldHeading,
+     * Admin_UI_FieldTable, Admin_UI_FieldGroup. <br>
+     * It removes all subelements of the field. like Properties and so on.
+     *
+     * @param _parameter Parameter as passed by eFaps to an esjp
+     * @return new Return
+     * @throws EFapsException on error
+     */
+    public Return execute(final Parameter _parameter)
+        throws EFapsException
+    {
+        final Instance instance = _parameter.getInstance();
 
-  /**
-   * Method is executed from the DELETE_PRE trigger of the type Admin_UI_Field,
-   * Admin_UI_FieldSet, Admin_UI_FieldHeading, Admin_UI_FieldTable,
-   * Admin_UI_FieldGroup. <br>
-   * It removes all subelements of the field. like Properties and so on.
-   * @param _parameter Parameter as passed by eFaps to an esjp
-   * @return new Return
-   * @throws EFapsException on error
-   */
-  public Return execute(final Parameter _parameter)
-      throws EFapsException {
-    final Instance instance = _parameter.getInstance();
+        // remove properties
+        final QueryBuilder propQueryBldr = new QueryBuilder(CIAdminCommon.Property);
+        propQueryBldr.addWhereAttrEqValue(CIAdminCommon.Property.Abstract, instance.getId());
+        final InstanceQuery propQuery = propQueryBldr.getQuery();
+        propQuery.executeWithoutAccessCheck();
+        while (propQuery.next()) {
+            final Delete del = new Delete(propQuery.getCurrentValue());
+            del.execute();
+        }
 
-    //remove properties
-    final SearchQuery query = new SearchQuery();
-    query.setExpand(instance, "Admin_Common_Property\\Abstract");
-    query.addSelect("OID");
-    query.execute();
-    while (query.next()) {
-      final Delete del = new Delete((String) query.get("OID"));
-      del.execute();
+        // remove links
+        final QueryBuilder linkQueryBldr = new QueryBuilder(CIAdminUserInterface.Link);
+        linkQueryBldr.addWhereAttrEqValue(CIAdminUserInterface.Link.From, instance.getId());
+        final InstanceQuery linkQuery = linkQueryBldr.getQuery();
+        linkQuery.executeWithoutAccessCheck();
+        while (linkQuery.next()) {
+            final Delete del = new Delete(linkQuery.getCurrentValue());
+            del.execute();
+        }
+
+        // remove events
+        final QueryBuilder eventQueryBldr = new QueryBuilder(CIAdminEvent.Definition);
+        eventQueryBldr.addWhereAttrEqValue(CIAdminEvent.Definition.Abstract, instance.getId());
+        final InstanceQuery eventQuery = eventQueryBldr.getQuery();
+        eventQuery.executeWithoutAccessCheck();
+        while (eventQuery.next()) {
+            final Delete del = new Delete(eventQuery.getCurrentValue());
+            del.execute();
+        }
+        return new Return();
     }
-    query.close();
-
-    //remove links
-    final SearchQuery linkQuery = new SearchQuery();
-    linkQuery.setExpand(instance, "Admin_UI_Link\\From");
-    linkQuery.addSelect("OID");
-    linkQuery.execute();
-    while (linkQuery.next()) {
-      final Delete del = new Delete((String) linkQuery.get("OID"));
-      del.execute();
-    }
-    linkQuery.close();
-
-    //remove events
-    final SearchQuery eventQuery = new SearchQuery();
-    eventQuery.setExpand(instance, "Admin_Event_Definition\\Abstract");
-    eventQuery.addSelect("OID");
-    eventQuery.execute();
-    while (eventQuery.next()) {
-      final Delete del = new Delete((String) eventQuery.get("OID"));
-      del.execute();
-    }
-    eventQuery.close();
-    return new Return();
-  }
 }
