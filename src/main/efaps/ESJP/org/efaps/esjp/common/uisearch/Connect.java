474efc271ffe823f51f3b6c9522c23e02c9627df
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2013-03-20 01:59:16 +0000

    - kernel-install: update of esjp for connect to be extendable
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-Kernel-Install/trunk@9103 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/common/uisearch/Connect.java
+++ src/main/efaps/ESJP/org/efaps/esjp/common/uisearch/Connect.java
@@ -20,72 +20,21 @@
 
 package org.efaps.esjp.common.uisearch;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.efaps.admin.datamodel.Type;
-import org.efaps.admin.event.EventExecution;
-import org.efaps.admin.event.Parameter;
-import org.efaps.admin.event.Parameter.ParameterValues;
-import org.efaps.admin.event.Return;
 import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
-import org.efaps.db.Insert;
-import org.efaps.db.Instance;
-import org.efaps.db.MultiPrintQuery;
-import org.efaps.db.QueryBuilder;
-import org.efaps.util.EFapsException;
 
 /**
+ * This class must be replaced for customization, therefore it is left empty.
+ * Functional description can be found in the related "<code>_Base</code>"
+ * class.
+ *
  * @author The eFaps Team
  * @version $Id$
  */
 @EFapsUUID("9993f975-855d-4d19-865a-c509bf410149")
 @EFapsRevision("$Rev$")
-public class Connect implements EventExecution
+public class Connect
+    extends Connect_Base
 {
-    /**
-     * @param _parameter Parameter as passed from the eFaps API
-     * @return new Return
-     * @throws EFapsException on error
-     */
-    public Return execute(final Parameter _parameter)
-        throws EFapsException
-    {
-        final Map<?, ?> properties = (Map<?, ?>) _parameter.get(ParameterValues.PROPERTIES);
-        final Instance parent = (Instance) _parameter.get(ParameterValues.INSTANCE);
-        final Map<?, ?> others = (HashMap<?, ?>) _parameter.get(ParameterValues.OTHERS);
 
-        final String[] childOids = (String[]) others.get("selectedRow");
-        if (childOids != null) {
-            final String type = (String) properties.get("ConnectType");
-            final String childAttr = (String) properties.get("ConnectChildAttribute");
-            final String parentAttr = (String) properties.get("ConnectParentAttribute");
-            final boolean allowAttr = "true".equals(properties.get("AllowMultiple"));
-
-            for (final String childOid : childOids) {
-                final Instance child = Instance.get(childOid);
-                boolean check = false;
-                if (allowAttr) {
-                    final QueryBuilder queryBldr = new QueryBuilder(Type.get(type));
-                    queryBldr.addWhereAttrEqValue(parentAttr, parent.getId());
-                    queryBldr.addWhereAttrEqValue(childAttr, child.getId());
-                    final MultiPrintQuery multi = queryBldr.getPrint();
-                    multi.execute();
-                    while (multi.next()) {
-                        check = true;
-                        break;
-                    }
-                }
-
-                if (!check) {
-                    final Insert insert = new Insert(type);
-                    insert.add(parentAttr, parent.getId());
-                    insert.add(childAttr, child.getId());
-                    insert.execute();
-                }
-            }
-        }
-        return new Return();
-    }
 }
