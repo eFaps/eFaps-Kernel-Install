f63faed709134b1a2337a402b6422845ed218581
sara.landa@efaps.org <sara.landa@efaps.org@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2013-05-21 18:54:31 +0000

    - Kernel-Install: class will be replaced by the MultiPrint and HistoryPrint in Webapp-install
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-Kernel-Install/trunk@9489 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/common/uitable/MultiPrint.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2003 - 2013 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev: 9459 $
+ * Last Changed:    $Date: 2013-05-17 15:09:07 -0500 (vie, 17 may 2013) $
+ * Last Changed By: $Author: jorge.cueva@moxter.net $
+ */
+
+
+package org.efaps.esjp.common.uitable;
+
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+
+
+/**
+ * This class will be replaced by the MultiPrint in Webapp-install. It serves
+ * as a placeholder to be able to link to the esjp during a first time installation.
+ *
+ * @author The eFaps Team
+ * @version $Id: MultiPrint.java 9459 2013-05-17 20:09:07Z jorge.cueva@moxter.net $
+ */
+@EFapsUUID("cbab3ae6-fe28-4604-838c-4c6d260156fb")
+@EFapsRevision("$Rev: 9459 $")
+public class MultiPrint
+{
+
+}
