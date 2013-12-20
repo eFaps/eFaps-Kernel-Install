1de870b848d314e57385f5d612a2c58d0c8bc8df
jorge.cueva@moxter.net <jorge.cueva@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2013-12-20 23:06:21 +0000

    - kernel-install: add  event definition class.
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-Kernel-Install/trunk@11495 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/common/quartz/EventDefinitionJob.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2003 - 2013 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev: 10208 $
+ * Last Changed:    $Date: 2013-09-16 16:16:22 -0500 (lun, 16 sep 2013) $
+ * Last Changed By: $Author: jan@moxter.net $
+ */
+
+package org.efaps.esjp.common.quartz;
+
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+
+/**
+ * TODO Comment.
+ *
+ * @author The eFaps Team
+ * @version $Id: EventDefinitionJob.java 10208 2013-09-16 21:16:22Z jan@moxter.net $
+ */
+@EFapsUUID("65e41ea6-f147-49a4-9b81-c34d2dd03f31")
+@EFapsRevision("$Rev: 10208 $")
+public class EventDefinitionJob
+    extends EventDefinitionJob_Base
+{
+}
---
+++ src/main/efaps/ESJP/org/efaps/esjp/common/quartz/EventDefinitionJob_Base.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2003 - 2013 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev: 10208 $
+ * Last Changed:    $Date: 2013-09-16 16:16:22 -0500 (lun, 16 sep 2013) $
+ * Last Changed By: $Author: jan@moxter.net $
+ */
+
+package org.efaps.esjp.common.quartz;
+
+import java.util.UUID;
+
+import org.efaps.admin.datamodel.Type;
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.ci.CIAdminProgram;
+import org.efaps.db.AttributeQuery;
+import org.efaps.db.MultiPrintQuery;
+import org.efaps.db.QueryBuilder;
+import org.efaps.db.SelectBuilder;
+import org.efaps.util.EFapsException;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ * TODO Comment.
+ *
+ * @author The eFaps Team
+ * @version $Id: EventDefinitionJob_Base.java 10208 2013-09-16 21:16:22Z jan@moxter.net $
+ */
+@EFapsUUID("9b423589-e341-45c0-b59f-098d6dd8006c")
+@EFapsRevision("$Rev: 10208 $")
+public abstract class EventDefinitionJob_Base
+    implements Job
+{
+    @Override
+    public void execute(final JobExecutionContext _jobExec)
+        throws JobExecutionException
+    {
+        final String[] quartzObj = _jobExec.getJobDetail().getKey().getName().split("_");
+        final String name = quartzObj[0];
+        final String clazz = quartzObj[1];
+
+        try {
+            final QueryBuilder attrQueryBldr = new QueryBuilder(CIAdminProgram.Java);
+            attrQueryBldr.addWhereAttrEqValue(CIAdminProgram.Java.Name, name);
+            final AttributeQuery attrQuery = attrQueryBldr.getAttributeQuery(CIAdminProgram.Java.ID);
+
+            // ERP_EventDefinitionAbstract
+            final QueryBuilder attrQueryBldr2 = new QueryBuilder(Type
+                            .get(UUID.fromString("847cf68b-c23a-4826-a154-dc38ff7f76fd")));
+            attrQueryBldr2.addWhereAttrInQuery("QuartzDefinitionLink", attrQuery);
+            final AttributeQuery attrQuery2 = attrQueryBldr2.getAttributeQuery("ID");
+
+            // ERP_EventDefinition2JavaProgram
+            final QueryBuilder queryBldr = new QueryBuilder(Type
+                            .get(UUID.fromString("b8405fb3-2dd1-440d-88bd-a64491fc27df")));
+            queryBldr.addWhereAttrInQuery("EventDefinitionLink", attrQuery2);
+            final MultiPrintQuery multi = queryBldr.getPrint();
+            final SelectBuilder selProgInst = new SelectBuilder().linkto("JavaProgramLink").instance();
+            multi.addSelect(selProgInst);
+            multi.executeWithoutAccessCheck();
+            while (multi.next()) {
+
+            }
+
+        } catch (final EFapsException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
