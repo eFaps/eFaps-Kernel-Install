50e5c6880ca668edeebf3b8483ae4b3e345e7e8e
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2014-07-02 23:10:03 +0000

    - kernel-install: update of basic connect esjp
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-Kernel-Install/trunk@13242 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/common/uisearch/Connect_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/common/uisearch/Connect_Base.java
@@ -23,8 +23,10 @@ package org.efaps.esjp.common.uisearch;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.UUID;
 
+import org.efaps.admin.datamodel.Attribute;
 import org.efaps.admin.datamodel.Type;
 import org.efaps.admin.event.EventExecution;
 import org.efaps.admin.event.Parameter;
@@ -68,46 +70,124 @@ public abstract class Connect_Base
     public Return execute(final Parameter _parameter)
         throws EFapsException
     {
-        final Instance parent = (Instance) _parameter.get(ParameterValues.INSTANCE);
+        final Instance parentInst = (Instance) _parameter.get(ParameterValues.INSTANCE);
         final Map<?, ?> others = (HashMap<?, ?>) _parameter.get(ParameterValues.OTHERS);
-
         final String[] childOids = (String[]) others.get("selectedRow");
         if (childOids != null) {
+            final Map<Integer, String> parentAttrs = analyseProperty(_parameter, "ConnectParentAttribute");
+            final Map<Integer, String> childAttrs = analyseProperty(_parameter, "ConnectChildAttribute");
+            final Map<Integer, String> childTypes = analyseProperty(_parameter, "ConnectChildType");
+            final Map<Integer, String> types = analyseProperty(_parameter, "ConnectType");
 
-            final String parentAttr = getProperty(_parameter, "ConnectParentAttribute");
-            final String childAttr = getProperty(_parameter, "ConnectChildAttribute");
-            final boolean allowAttr = "false".equals(getProperty(_parameter, "AllowMultiple"));
-            if (parentAttr == null || childAttr == null) {
-                Connect_Base.LOG.error("Missing properties 'ConnectParentAttribute' or 'ConnectChildAttribute'");
-            }
-            for (final String childOid : childOids) {
-                final Instance child = Instance.get(childOid);
-                if (child.isValid()) {
-                    final Type type = getConnectType(_parameter, child);
-                    if (type != null) {
-                        boolean check = false;
-                        if (allowAttr) {
-                            final QueryBuilder queryBldr = new QueryBuilder(type);
-                            queryBldr.addWhereAttrEqValue(parentAttr, parent.getId());
-                            queryBldr.addWhereAttrEqValue(childAttr, child.getId());
-                            final InstanceQuery query = queryBldr.getQuery();
-                            query.executeWithoutAccessCheck();
-                            if (query.next()) {
-                                check = true;
+            final boolean checkMultiple = "false".equals(getProperty(_parameter, "AllowMultiple"));
+            if (parentAttrs.isEmpty() || childAttrs.isEmpty() || (parentAttrs.size() != childAttrs.size())
+                            || types.isEmpty()) {
+                Connect_Base.LOG.error("Must have properties 'ConnectParentAttribute' and 'ConnectChildAttribute' "
+                                + "of same size and at least one 'ConnectType'");
+            } else {
+                for (final String childOid : childOids) {
+                    final Instance childInst = Instance.get(childOid);
+                    if (childInst.isValid()) {
+                        final Type type = getConnectType(_parameter, childInst, childTypes, types);
+                        if (type != null) {
+                            boolean check = false;
+                            final Attribute parentAttr = getAttr(_parameter, type, types, parentAttrs);
+                            final Attribute childAttr = getAttr(_parameter, type, types, childAttrs);
+                            if (checkMultiple) {
+                                final QueryBuilder queryBldr = new QueryBuilder(type);
+                                queryBldr.addWhereAttrEqValue(parentAttr, parentInst);
+                                queryBldr.addWhereAttrEqValue(childAttr, childInst);
+                                final InstanceQuery query = queryBldr.getQuery();
+                                query.executeWithoutAccessCheck();
+                                if (query.next()) {
+                                    check = true;
+                                }
                             }
-                        }
-                        if (!check) {
-                            final Insert insert = new Insert(type);
-                            addInsertConnect(_parameter, insert);
-                            insert.add(parentAttr, parent.getId());
-                            insert.add(childAttr, child.getId());
-                            insert.execute();
+                            if (!check) {
+                                final Insert insert = new Insert(type);
+                                addInsertConnect(_parameter, insert);
+                                insert.add(parentAttr, parentInst);
+                                insert.add(childAttr, childInst);
+                                insert.execute();
+                            }
                         }
                     }
                 }
             }
         }
         return new Return();
+    }
+
+    /**
+     * @param _parameter Parameter as passed by the eFaps API
+     * @param _type    instance to be connected
+     * @param _types mapping of childTypes
+     * @param _attrs    mapping of types
+     * @return the attribute
+     * @throws EFapsException on error
+     */
+    protected Attribute getAttr(final Parameter _parameter,
+                                final Type _type,
+                                final Map<Integer, String> _types,
+                                final Map<Integer, String> _attrs)
+        throws EFapsException
+    {
+        Attribute ret = null;
+        // simple version without any mapping
+        if (_types.size() == 1) {
+            final String attrStr = _attrs.values().iterator().next();
+            ret = _type.getAttribute(attrStr);
+        } else {
+            for (final Entry<Integer, String> entry : _types.entrySet()) {
+                final String typeStr = entry.getValue();
+                final Type type = isUUID(typeStr) ? Type.get(UUID.fromString(typeStr)) : Type.get(typeStr);
+                if (_type.equals(type)) {
+                    final String attrStr;
+                    if (_attrs.containsKey(entry.getKey())) {
+                        attrStr = _attrs.get(entry.getKey());
+                    } else {
+                        attrStr = _attrs.values().iterator().next();
+                    }
+                    ret = _type.getAttribute(attrStr);
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
+
+    /**
+     * @param _parameter Parameter as passed by the eFaps API
+     * @param _childInst    instance to be connected
+     * @param _childTypes mapping of childTypes
+     * @param _types    mapping of types
+     * @return the type
+     * @throws EFapsException on error
+     */
+    protected Type getConnectType(final Parameter _parameter,
+                                  final Instance _childInst,
+                                  final Map<Integer, String> _childTypes,
+                                  final Map<Integer, String> _types)
+        throws EFapsException
+    {
+        Type ret = null;
+        // simple version without any mapping
+        if (_childTypes.isEmpty()) {
+            final String typeStr = _types.values().iterator().next();
+            ret = isUUID(typeStr) ? Type.get(UUID.fromString(typeStr)) : Type.get(typeStr);
+        } else {
+            for (final Entry<Integer, String> entry : _childTypes.entrySet()) {
+                final String childTypeStr = entry.getValue();
+                final Type childType = isUUID(childTypeStr) ? Type.get(UUID.fromString(childTypeStr)) : Type
+                                .get(childTypeStr);
+                if (_childInst.getType().equals(childType)) {
+                    final String typeStr = _types.get(entry.getKey());
+                    ret = isUUID(typeStr) ? Type.get(UUID.fromString(typeStr)) : Type.get(typeStr);
+                    break;
+                }
+            }
+        }
+        return ret;
     }
 
     /**
@@ -121,35 +201,5 @@ public abstract class Connect_Base
         throws EFapsException
     {
         // to implement
-    }
-
-    /**
-     * Get the connect type.
-     * Can be used with a concatenated mapping of
-     * 'UUID from ChildObject';'UUID from MiddleObject'
-     * @param _parameter Parameter as passed by the eFaps API
-     * @param _childInstance Instance of the child that will be connected
-     * @return the type used for the connection
-     * @throws EFapsException on error
-     */
-    protected Type getConnectType(final Parameter _parameter,
-                                  final Instance _childInstance)
-        throws EFapsException
-    {
-        final String typeStr = getProperty(_parameter, "ConnectType");
-        Type ret = null;
-        // if no simple type, check for a mapping
-        if (typeStr == null) {
-            final Map<Integer, String> mapping = analyseProperty(_parameter, "ConnectTypeMap");
-            for (final String typeMap : mapping.values()) {
-                final String[] uuids = typeMap.split(";");
-                if (_childInstance.getType().getUUID().equals(UUID.fromString(uuids[0]))) {
-                    ret = Type.get(UUID.fromString(uuids[1]));
-                }
-            }
-        } else {
-            ret  = Type.get(typeStr);
-        }
-        return ret;
     }
 }
