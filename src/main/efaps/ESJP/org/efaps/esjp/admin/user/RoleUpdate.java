757ebb4bc93750b9b09b66a1cdc289fe05ca5b77
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2014-09-05 15:20:11 +0000

    - kernel-install: update of RoleUpdate basic esjp
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-Kernel-Install/trunk@13942 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/admin/user/RoleUpdate.java
+++ src/main/efaps/ESJP/org/efaps/esjp/admin/user/RoleUpdate.java
@@ -27,12 +27,14 @@ import org.efaps.admin.datamodel.Type;
 import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
 import org.efaps.admin.user.Role;
+import org.efaps.ci.CIAdminAccess;
 import org.efaps.ci.CIAdminUserInterface;
 import org.efaps.db.Delete;
 import org.efaps.db.Insert;
 import org.efaps.db.Instance;
 import org.efaps.db.InstanceQuery;
 import org.efaps.db.QueryBuilder;
+import org.efaps.esjp.common.AbstractCommon;
 import org.efaps.util.EFapsException;
 import org.efaps.util.cache.CacheReloadException;
 import org.slf4j.Logger;
@@ -48,6 +50,7 @@ import org.slf4j.LoggerFactory;
 @EFapsUUID("34badd85-b416-46fc-89ff-0f1e54c1eb9a")
 @EFapsRevision("$Rev$")
 public class RoleUpdate
+    extends AbstractCommon
 {
 
     /**
@@ -61,27 +64,31 @@ public class RoleUpdate
     private final Role role;
 
     /**
-     * @param _uuid UUID of the role to be updated
+     * @param _key UUID/Name of the role to be updated
      * @throws CacheReloadException on error
      */
-    public RoleUpdate(final String _uuid)
+    public RoleUpdate(final String _key)
         throws CacheReloadException
     {
-        RoleUpdate.LOG.debug("start RoleUpdate for UUID '{}'", _uuid);
-        this.role = Role.get(UUID.fromString(_uuid));
+        RoleUpdate.LOG.debug("start RoleUpdate for UUID/Name '{}'", _key);
+        this.role = isUUID(_key) ? Role.get(UUID.fromString(_key)) : Role.get(_key);
         RoleUpdate.LOG.debug("initialized RoleUpdate for Role '{}'", this.role.getName());
     }
 
     /**
-     * @param _uuid the UserIntferace Object to be connected to this role
+     * @param _key the UserIntferace Object to be connected to this role
      * @throws EFapsException on error
      */
-    public void addUI(final String _uuid)
+    public void addUI(final String _key)
         throws EFapsException
     {
-        RoleUpdate.LOG.debug("adding UserInterface '{}'", _uuid);
+        RoleUpdate.LOG.debug("adding UserInterface '{}'", _key);
         final QueryBuilder queryBldr = new QueryBuilder(CIAdminUserInterface.Direct);
-        queryBldr.addWhereAttrEqValue(CIAdminUserInterface.Direct.UUID, _uuid);
+        if (isUUID(_key)) {
+            queryBldr.addWhereAttrEqValue(CIAdminUserInterface.Direct.UUID, _key);
+        } else {
+            queryBldr.addWhereAttrEqValue(CIAdminUserInterface.Direct.Name, _key);
+        }
         final InstanceQuery query = queryBldr.getQuery();
         query.execute();
         if (query.next()) {
@@ -91,13 +98,13 @@ public class RoleUpdate
                 final QueryBuilder accQueryBldr = new QueryBuilder(Type.get(
                                 UUID.fromString("7e59cebf-21ff-4ee1-8140-44139c90a658")));
                 accQueryBldr.addWhereAttrEqValue("UserLink", this.role.getId());
-                accQueryBldr.addWhereAttrEqValue("UILink", uiInst.getId());
+                accQueryBldr.addWhereAttrEqValue("UILink", uiInst);
                 final InstanceQuery accQuery = accQueryBldr.getQuery();
                 if (accQuery.execute().isEmpty()) {
                     //Admin_UI_Access
                     final Insert insert = new Insert(Type.get(UUID.fromString("7e59cebf-21ff-4ee1-8140-44139c90a658")));
                     insert.add("UserLink", this.role.getId());
-                    insert.add("UILink", uiInst.getId());
+                    insert.add("UILink", uiInst);
                     insert.execute();
                 }
             }
@@ -105,25 +112,98 @@ public class RoleUpdate
     }
 
     /**
-     * @param _uuid the UserIntferace Object to be removed from this role
+     * @param _key the UserIntferace Object to be removed from this role
      * @throws EFapsException on error
      */
-    public void removeUI(final String _uuid)
+    public void removeUI(final String _key)
         throws EFapsException
     {
-        RoleUpdate.LOG.debug("removing UserInterface '{}'", _uuid);
+        RoleUpdate.LOG.debug("removing UserInterface '{}'", _key);
         final QueryBuilder queryBldr = new QueryBuilder(CIAdminUserInterface.Direct);
-        queryBldr.addWhereAttrEqValue(CIAdminUserInterface.Direct.UUID, _uuid);
+        if (isUUID(_key)) {
+            queryBldr.addWhereAttrEqValue(CIAdminUserInterface.Direct.UUID, _key);
+        } else {
+            queryBldr.addWhereAttrEqValue(CIAdminUserInterface.Direct.Name, _key);
+        }
         final InstanceQuery query = queryBldr.getQuery();
         query.execute();
         query.next();
         final Instance uiInst = query.getCurrentValue();
         if (uiInst.isValid()) {
-          //Admin_UI_Access
+            //Admin_UI_Access
             final QueryBuilder relQueryBldr = new QueryBuilder(
                             Type.get(UUID.fromString("7e59cebf-21ff-4ee1-8140-44139c90a658")));
             relQueryBldr.addWhereAttrEqValue("UserLink", this.role.getId());
-            relQueryBldr.addWhereAttrEqValue("UILink", uiInst.getId());
+            relQueryBldr.addWhereAttrEqValue("UILink", uiInst);
+            final InstanceQuery relQuery = relQueryBldr.getQuery();
+            relQuery.execute();
+            if (relQuery.next()) {
+                final Delete del = new Delete(relQuery.getCurrentValue());
+                del.execute();
+            }
+        }
+    }
+
+    /**
+     * @param _key the AccessSet to be connected to this role
+     * @throws EFapsException on error
+     */
+    public void addAccessSet(final String _key)
+        throws EFapsException
+    {
+        RoleUpdate.LOG.debug("adding AccessSet '{}'", _key);
+        final QueryBuilder queryBldr = new QueryBuilder(CIAdminAccess.AccessSet);
+        if (isUUID(_key)) {
+            queryBldr.addWhereAttrEqValue(CIAdminAccess.AccessSet.UUID, _key);
+        } else {
+            queryBldr.addWhereAttrEqValue(CIAdminAccess.AccessSet.Name, _key);
+        }
+        final InstanceQuery query = queryBldr.getQuery();
+        query.execute();
+        if (query.next()) {
+            final Instance uiInst = query.getCurrentValue();
+            if (uiInst.isValid()) {
+                //Admin_Access_AccessSet2UserAbstract
+                final QueryBuilder accQueryBldr = new QueryBuilder(Type.get(
+                                UUID.fromString("92975695-7a98-4221-bc9a-75c3dcb0e152")));
+                accQueryBldr.addWhereAttrEqValue("UserAbstractLink", this.role.getId());
+                accQueryBldr.addWhereAttrEqValue("AccessSetLink", uiInst);
+                final InstanceQuery accQuery = accQueryBldr.getQuery();
+                if (accQuery.execute().isEmpty()) {
+                    //Admin_Access_AccessSet2UserAbstract
+                    final Insert insert = new Insert(Type.get(UUID.fromString("92975695-7a98-4221-bc9a-75c3dcb0e152")));
+                    insert.add("UserAbstractLink", this.role.getId());
+                    insert.add("AccessSetLink", uiInst);
+                    insert.execute();
+                }
+            }
+        }
+    }
+
+    /**
+     * @param _key the AccessSet to be removed from this role
+     * @throws EFapsException on error
+     */
+    public void removeAccessSet(final String _key)
+        throws EFapsException
+    {
+        RoleUpdate.LOG.debug("removing AccessSet '{}'", _key);
+        final QueryBuilder queryBldr = new QueryBuilder(CIAdminAccess.AccessSet);
+        if (isUUID(_key)) {
+            queryBldr.addWhereAttrEqValue(CIAdminAccess.AccessSet.UUID, _key);
+        } else {
+            queryBldr.addWhereAttrEqValue(CIAdminAccess.AccessSet.Name, _key);
+        }
+        final InstanceQuery query = queryBldr.getQuery();
+        query.execute();
+        query.next();
+        final Instance uiInst = query.getCurrentValue();
+        if (uiInst.isValid()) {
+            //Admin_Access_AccessSet2UserAbstract
+            final QueryBuilder relQueryBldr = new QueryBuilder(
+                            Type.get(UUID.fromString("92975695-7a98-4221-bc9a-75c3dcb0e152")));
+            relQueryBldr.addWhereAttrEqValue("UserAbstractLink", this.role.getId());
+            relQueryBldr.addWhereAttrEqValue("AccessSetLink", uiInst);
             final InstanceQuery relQuery = relQueryBldr.getQuery();
             relQuery.execute();
             if (relQuery.next()) {
