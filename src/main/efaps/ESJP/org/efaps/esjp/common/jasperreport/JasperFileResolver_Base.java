0f18ad88613629ba4af76148e1e54ad639826e30
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2014-11-05 21:30:52 +0000

    - kernel-install: adding functionalities to jasper file resolver
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-Kernel-Install/trunk@14374 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/common/jasperreport/JasperFileResolver_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/common/jasperreport/JasperFileResolver_Base.java
@@ -25,6 +25,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import net.sf.jasperreports.engine.util.FileResolver;
 
@@ -48,54 +50,70 @@ import org.slf4j.LoggerFactory;
  * <li>Images (JasperImages)</li>
  * </ul>
  * To now if a image or a subreport is wanted a naming convention is used.
- *  <ul>
- *    <li>Images must start with "JasperImage."</li>
- *    <li>JasperReports must start with "JasperReport."</li>
- *  </ul>
- *  <b>Caution: The dot is expected!</b>
+ * <ul>
+ * <li>Images must start with "JasperImage."</li>
+ * <li>JasperReports must start with "JasperReport."</li>
+ * </ul>
+ * <b>Caution: The dot is expected!</b>
  *
  * @author The eFaps Team
- * @version $Id$
+ * @version $Id: JasperFileResolver_Base.java 8885 2013-02-20 00:32:07Z
+ *          jan@moxter.net $
  */
 @EFapsUUID("4733dd43-2ef3-4572-a1e9-c820567e9a36")
 @EFapsRevision("$Rev$")
 public abstract class JasperFileResolver_Base
     implements FileResolver
 {
+
     /**
      * Logging instance used to give logging information of this class.
      */
-    protected static final Logger LOG = LoggerFactory.getLogger(JasperFileResolver_Base.class);
+    private static final Logger LOG = LoggerFactory.getLogger(JasperFileResolver.class);
+
+    /**
+     * Pattern for the filename in case of jasper.
+     */
+    private static Pattern reportPattern = Pattern.compile("([A-Z,a-z,_,0-9])+(?=\\.)");
 
     /**
      * @see net.sf.jasperreports.engine.util.FileResolver#resolveFile(java.lang.String)
      * @param _jasperFileName name of the jasper report
      * @return File
      */
+    @Override
     public File resolveFile(final String _jasperFileName)
     {
         File file = null;
         try {
-            final QueryBuilder queryBldr;
+            QueryBuilder queryBldr = null;
             String name = null;
             if (_jasperFileName.startsWith("JasperImage.")) {
                 name = _jasperFileName.replace("JasperImage.", "");
                 queryBldr = new QueryBuilder(CIAdminProgram.JasperImage);
-            } else {
+            } else if (_jasperFileName.startsWith("JasperReport.")) {
                 name = _jasperFileName.replace("JasperReport.", "");
                 queryBldr = new QueryBuilder(CIAdminProgram.JasperReportCompiled);
+            } else if (_jasperFileName.endsWith("jasper") || _jasperFileName.endsWith("jrxml")) {
+                final Matcher matcher = reportPattern.matcher(_jasperFileName);
+                if (matcher.find()) {
+                    name = matcher.group();
+                    queryBldr = new QueryBuilder(CIAdminProgram.JasperReportCompiled);
+                }
             }
-            queryBldr.addWhereAttrEqValue("Name", name);
-            final InstanceQuery query = queryBldr.getQuery();
-            query.execute();
-            if (query.next()) {
-                final Checkout checkout = new Checkout(query.getCurrentValue());
-                final InputStream input = checkout.execute();
-                file = new FileUtil().getFile(checkout.getFileName(), "jasper");
-                final OutputStream out = new FileOutputStream(file);
-                IOUtils.copy(input, out);
-                input.close();
-                out.close();
+            if (queryBldr != null) {
+                queryBldr.addWhereAttrEqValue("Name", name);
+                final InstanceQuery query = queryBldr.getQuery();
+                query.execute();
+                if (query.next()) {
+                    final Checkout checkout = new Checkout(query.getCurrentValue());
+                    final InputStream input = checkout.execute();
+                    file = new FileUtil().getFile(checkout.getFileName(), "jasper");
+                    final OutputStream out = new FileOutputStream(file);
+                    IOUtils.copy(input, out);
+                    input.close();
+                    out.close();
+                }
             }
         } catch (final EFapsException e) {
             JasperFileResolver_Base.LOG.error("error in JasperFileResolver.resolveFile", e);
