51654652987caaed7d531e3afa4cee07823a662b
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2014-11-28 16:24:25 +0000

    - kernel-install: update of file checkout
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-Kernel-Install/trunk@14541 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/common/file/FileCheckout.java
+++ src/main/efaps/ESJP/org/efaps/esjp/common/file/FileCheckout.java
@@ -25,15 +25,20 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.io.IOUtils;
 import org.efaps.admin.event.EventExecution;
 import org.efaps.admin.event.Parameter;
+import org.efaps.admin.event.Parameter.ParameterValues;
 import org.efaps.admin.event.Return;
 import org.efaps.admin.event.Return.ReturnValues;
 import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
 import org.efaps.db.Checkout;
+import org.efaps.db.Instance;
+import org.efaps.esjp.common.AbstractCommon;
 import org.efaps.util.EFapsException;
 
 /**
@@ -46,6 +51,7 @@ import org.efaps.util.EFapsException;
 @EFapsUUID("d9ba2b85-8b9a-46b0-929e-8e938e7d5577")
 @EFapsRevision("$Rev$")
 public class FileCheckout
+    extends AbstractCommon
     implements EventExecution
 {
 
@@ -72,4 +78,46 @@ public class FileCheckout
         ret.put(ReturnValues.VALUES, temp);
         return ret;
     }
+
+    /**
+     * @param _parameter Parameter as passed by the eFaps API
+     * @return return combing
+     * @throws EFapsException on error
+     */
+    public Return multiple(final Parameter _parameter)
+        throws EFapsException
+    {
+        File retFile = null;
+        try {
+            final String[] oids = (String[]) _parameter.get(ParameterValues.OTHERS);
+            if (oids != null) {
+                final List<Instance> instances = new ArrayList<>();
+                for (final String oid : oids) {
+                    final Instance instance = Instance.get(oid);
+                    if (instance.isValid()) {
+                        instances.add(instance);
+                    }
+                }
+                final List<File> files = new ArrayList<>();
+                for (final Instance instance : instances) {
+                    final Checkout checkout = new Checkout(instance);
+                    final InputStream input = checkout.execute();
+                    final File temp = new FileUtil().getFile(checkout.getFileName());
+                    final OutputStream out = new FileOutputStream(temp);
+                    IOUtils.copy(input, out);
+                    input.close();
+                    out.close();
+                    files.add(temp);
+                }
+                retFile = new FileUtil().combinePdfs(files, getProperty(_parameter, "FileName", "CheckOut"), false);
+            }
+        } catch (final IOException e) {
+            throw new EFapsException(FileCheckout.class, "IOException", e);
+        }
+        final Return ret = new Return();
+        ret.put(ReturnValues.VALUES, retFile);
+        return ret;
+    }
 }
+
+
