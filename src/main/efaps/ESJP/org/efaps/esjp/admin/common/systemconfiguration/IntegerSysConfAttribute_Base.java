0c3035093906c924a7eba4f61fd35f771971412f
Jan Moxter <jan.moxter@innobix.com>
2015-10-13 19:24:54 -0500

    - Issue #32: Add IntegerSysConfAttribute
    closes #32

---
+++ src/main/efaps/ESJP/org/efaps/esjp/admin/common/systemconfiguration/IntegerSysConfAttribute_Base.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2003 - 2015 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.efaps.esjp.admin.common.systemconfiguration;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.efaps.admin.common.SystemConfiguration;
+import org.efaps.admin.event.Parameter;
+import org.efaps.admin.program.esjp.EFapsApplication;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.util.EFapsException;
+
+/**
+ * The Class StringSysConfAttribute_Base.
+ *
+ * @author The eFaps Team
+ */
+@EFapsUUID("e0f6ee47-2a52-4ef8-88a3-955d9f31d5b5")
+@EFapsApplication("eFaps-Kernel")
+public abstract class IntegerSysConfAttribute_Base
+    extends AbstractSysConfAttribute<IntegerSysConfAttribute, Integer>
+{
+
+    @Override
+    protected IntegerSysConfAttribute getThis()
+    {
+        return (IntegerSysConfAttribute) this;
+    }
+
+    @Override
+    public Integer get()
+        throws EFapsException
+    {
+        return SystemConfiguration.get(getSysConfUUID()).getAttributeValueAsInteger(getKey());
+    }
+
+    @Override
+    public CharSequence getHtml(final Parameter _parameter,
+                                final Object _value)
+    {
+        final StringBuilder ret = new StringBuilder()
+                        .append("<input type=\"number\" name=\"value\" size=\"5\"");
+        if (_value != null) {
+            ret.append(" value=\"").append(StringEscapeUtils.escapeHtml4((String) _value)).append("\"");
+        }
+        ret.append(">");
+        return ret;
+    }
+}
