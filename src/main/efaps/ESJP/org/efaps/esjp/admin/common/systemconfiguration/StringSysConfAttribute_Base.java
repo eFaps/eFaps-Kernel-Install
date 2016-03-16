1f699a8d234ac3dabf8d69e3b6581be2c8e5ae8e
Jan Moxter <jan.moxter@innobix.com>
2016-03-16 13:36:15 -0500

    - Issue #62: StringSysConfAttribute must use the default value
    closes #62

--- src/main/efaps/ESJP/org/efaps/esjp/admin/common/systemconfiguration/StringSysConfAttribute_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/admin/common/systemconfiguration/StringSysConfAttribute_Base.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003 - 2015 The eFaps Team
+ * Copyright 2003 - 2016 The eFaps Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,7 +44,13 @@ public abstract class StringSysConfAttribute_Base
     public String get()
         throws EFapsException
     {
-        return SystemConfiguration.get(getSysConfUUID()).getAttributeValue(getKey());
+        final String ret;
+        if (SystemConfiguration.get(getSysConfUUID()).containsAttributeValue(getKey())) {
+            ret = SystemConfiguration.get(getSysConfUUID()).getAttributeValue(getKey());
+        } else {
+            ret = getDefaultValue();
+        }
+        return ret;
     }
 
     @Override
