300c36365607bb1d1ecd0a5dbd8d4da9d637f5a5
Jan Moxter <jan.moxter@innobix.com>
2016-07-12 17:54:19 -0500

    - Issue #104: Add a possibility to load a execute an CIItems Pack generated by the maven plugin

--- src/main/efaps/ESJP/org/efaps/esjp/admin/util/RevisionUtil.java
+++ src/main/efaps/ESJP/org/efaps/esjp/admin/util/RevisionUtil.java
@@ -18,10 +18,22 @@ package org.efaps.esjp.admin.util;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.efaps.admin.AppConfigHandler;
 import org.efaps.admin.event.Parameter;
 import org.efaps.admin.event.Return;
 import org.efaps.admin.program.esjp.EFapsApplication;
@@ -31,6 +43,10 @@ import org.efaps.ci.CIAdminCommon;
 import org.efaps.db.MultiPrintQuery;
 import org.efaps.db.QueryBuilder;
 import org.efaps.db.SelectBuilder;
+import org.efaps.rest.Update;
+import org.efaps.update.Install;
+import org.efaps.update.Install.InstallFile;
+import org.efaps.update.util.InstallationException;
 import org.efaps.util.EFapsException;
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
@@ -60,16 +76,48 @@ public class RevisionUtil
      * @param _parameter Parameter as passed by the eFaps API
      * @return the return
      * @throws EFapsException on error
+     * @throws InstallationException on error
      */
     public Return checkRevisions(final Parameter _parameter)
-        throws EFapsException
+        throws EFapsException, InstallationException
     {
-        final File file = new File("/home/janmoxter/git/RudolfReimsac/install/target/revisions.json");
 
-        try {
+        try (final FileInputStream in = new FileInputStream(new File(
+                        "/home/janmoxter/git/FonPeru/install/target/CIItems.tar"));
+                        final TarArchiveInputStream tarInput = new TarArchiveInputStream(in);) {
+
+            File tmpfld = AppConfigHandler.get().getTempFolder();
+            if (tmpfld == null) {
+                final File temp = File.createTempFile("eFaps", ".tmp");
+                tmpfld = temp.getParentFile();
+                temp.delete();
+            }
+            final File updateFolder = new File(tmpfld, Update.TMPFOLDERNAME);
+            if (!updateFolder.exists()) {
+                updateFolder.mkdirs();
+            }
+            final File dateFolder = new File(updateFolder, ((Long) new Date().getTime()).toString());
+            dateFolder.mkdirs();
+
+            final Map<String, URL> files = new HashMap<>();
+            TarArchiveEntry currentEntry = tarInput.getNextTarEntry();
+            while (currentEntry != null) {
+                final byte[] bytess = new byte[(int) currentEntry.getSize()];
+                tarInput.read(bytess);
+                final File file = new File(dateFolder, currentEntry.getName());
+                final FileOutputStream output = new FileOutputStream(file);
+                output.write(bytess);
+                output.close();
+                files.put(currentEntry.getName(), file.toURI().toURL());
+                currentEntry = tarInput.getNextTarEntry();
+            }
+
+            final List<InstallFile> installFiles = new ArrayList<>();
+
+            final URL json = files.get("revisions.json");
             final ObjectMapper mapper = new ObjectMapper();
             mapper.registerModule(new JodaModule());
-            final List<RevItem> items = mapper.readValue(new FileInputStream(file), mapper.getTypeFactory()
+            final List<RevItem> items = mapper.readValue(new File(json.toURI()), mapper.getTypeFactory()
                             .constructCollectionType(List.class, RevItem.class));
             int i = 1;
             for (final RevItem item : items) {
@@ -88,29 +136,63 @@ public class RevisionUtil
                 multi.addSelect(selRevision, selApp, selRevDate);
                 multi.addAttribute(CIAdmin.Abstract.Name);
                 multi.execute();
+                boolean update = false;
+                String name = "NEWELEMENT";
                 if (multi.next()) {
                     final String revision = multi.getSelect(selRevision);
                     final DateTime revDate = multi.getSelect(selRevDate);
                     final String app = multi.getSelect(selApp);
+                    name = multi.getAttribute(CIAdmin.Abstract.Name);
                     LOG.info("Found obj: {}, type: {}, name: {},  app: {}, date: {}, revision: {} ",
                                     multi.getCurrentInstance().getOid(),
                                     multi.getCurrentInstance().getType().getName(),
-                                    multi.getAttribute(CIAdmin.Abstract.Name),
-                                    app,
-                                    revDate,
-                                    revision);
+                                    name, app, revDate, revision);
                     if (!item.getApplication().equals(app)) {
                         LOG.warn("Different Application: {} - {}", item.getApplication(), app);
+                        update = true;
                     }
                     if (!item.getRevision().equals(revision)) {
                         LOG.warn("Different Revision: {} - {}", item.getRevision(), revision);
+                        update = true;
                     }
                 } else {
                     LOG.warn("Could not find item: {} ", item);
+                    update = true;
+                }
+                if (update) {
+                    final InstallFile installFile = new InstallFile()
+                                    .setName(name)
+                                    .setURL(files.get(item.getUuid() + ".xml"))
+                                    .setType("install-xml")
+                                    .setRevision(item.getRevision())
+                                    .setDate(item.getDate());
+                    installFiles.add(installFile);
                 }
                 i++;
             }
+            Collections.sort(installFiles, new Comparator<InstallFile>()
+            {
+
+                @Override
+                public int compare(final InstallFile _installFile0,
+                                   final InstallFile _installFile1)
+                {
+                    return _installFile0.getName().compareTo(_installFile1.getName());
+                }
+            });
+
+            if (!installFiles.isEmpty()) {
+                final Install install = new Install(true);
+                for (final InstallFile installFile : installFiles) {
+                    LOG.info("...Adding to Update: '{}' ", installFile.getName());
+                    install.addFile(installFile);
+                }
+                install.updateLatest(null);
+            }
+            LOG.info("Terminated update.");
         } catch (final IOException e) {
+            LOG.error("Catched", e);
+        } catch (final URISyntaxException e) {
             LOG.error("Catched", e);
         }
         return new Return();
