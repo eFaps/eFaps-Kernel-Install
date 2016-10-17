890906f1270016cc81d8cb0ae29e26b407d0ef72
Jan Moxter <jan.moxter@innobix.com>
2016-10-17 16:51:17 -0500

    - Issue #119: Add an access check for UserInterface that uses a Properties SystemConfiguration
    closes #119

--- src/main/efaps/ESJP/org/efaps/esjp/admin/access/AccessCheck4UI_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/admin/access/AccessCheck4UI_Base.java
@@ -398,6 +398,19 @@ public abstract class AccessCheck4UI_Base
                 }
             }
 
+            final Map<Integer, String> propAttrs = analyseProperty(_parameter, "PropertiesAttribute");
+            final Map<Integer, String> propKeys = analyseProperty(_parameter, "PropertiesKey");
+            for (final Entry<Integer, String> entry : propAttrs.entrySet()) {
+                final Properties props = config.getAttributeValueAsProperties(entry.getValue(), true);
+                final String propKey = propKeys.get(entry.getKey());
+                final String propKeyValue = props.getProperty(propKey.startsWith("!") ? propKey.substring(1) : propKey);
+                access = propKey.startsWith("!") ? !Boolean.parseBoolean(propKeyValue)
+                                : Boolean.parseBoolean(propKeyValue);
+                if (access) {
+                    break;
+                }
+            }
+
             final boolean inverse = "true".equalsIgnoreCase(getProperty(_parameter, "Inverse"));
             if (!inverse && access || inverse && !access) {
                 ret.put(ReturnValues.TRUE, true);
