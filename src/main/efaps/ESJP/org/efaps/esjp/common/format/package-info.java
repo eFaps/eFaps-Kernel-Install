bde74dbb3218a0da1842c068e355348d194d11ec
Jan Moxter <jan@moxter.net>
2024-02-01 21:19:09 -0500

    Set header

--- src/main/efaps/ESJP/org/efaps/esjp/common/format/package-info.java
+++ src/main/efaps/ESJP/org/efaps/esjp/common/format/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003 - 2014 The eFaps Team
+ * Copyright © 2003 - 2024 The eFaps Team (-)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,12 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * Revision:        $Rev: 14655 $
- * Last Changed:    $Date: 2014-12-20 13:13:11 -0500 (Sat, 20 Dec 2014) $
- * Last Changed By: $Author: jan@moxter.net $
  */
-
 /**
  * Classes used for formatting in Context with MsgPhrase.
  *
--- src/main/efaps/ESJP/org/efaps/esjp/common/uisearch/Search_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/common/uisearch/Search_Base.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003 - 2016 The eFaps Team
+ * Copyright © 2003 - 2024 The eFaps Team (-)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,12 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * Revision:        $Rev:1563 $
- * Last Changed:    $Date:2007-10-28 15:07:41 +0100 (So, 28 Okt 2007) $
- * Last Changed By: $Author:tmo $
  */
-
 package org.efaps.esjp.common.uisearch;
 
 import java.util.ArrayList;
